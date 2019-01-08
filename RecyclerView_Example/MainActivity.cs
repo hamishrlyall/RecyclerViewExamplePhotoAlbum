b6d610684f7a7935eb0076772efe039a2ce5ed0d
HAMISH LYALL <101614208@student.swin.edu.au>
2019-01-08 21:32:38 +1100

    Add project files.

---
+++ RecyclerView_Example/MainActivity.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+using System.Collections.Generic;
+using RecyclerViewer;
+
+namespace RecyclerView_Example
+{
+    [Activity(Label = "WallpaperApp", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        //Instances of components required to display the album in activityMain
+        //RecyclerView instance displays the album while the LayoutManager lays out each card in the RecyclerView
+        //PhotoAlbumAdapter that accesses and manages the PhotoAlbum
+        RecyclerView mRecyclerView;
+        RecyclerView.LayoutManager mLayoutManager;
+        PhotoAlbumAdapter mAdapter;
+        PhotoAlbum mPhotoAlbum;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            //Instantiates PhotoAlbum, Sets view to "activityMain", gets RecyclerView layout
+            mPhotoAlbum = new PhotoAlbum();
+            SetContentView(Resource.Layout.activity_main);
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+
+            //Plugs the layout manager into the RecyclerView
+            mLayoutManager = new LinearLayoutManager(this);
+            mRecyclerView.SetLayoutManager(mLayoutManager);
+
+            //Creates the PhotAlbumAdapter for the RecyclerView and passes the PhotoAlbum for it to manage
+            mAdapter = new PhotoAlbumAdapter(mPhotoAlbum);
+            mRecyclerView.SetAdapter(mAdapter);
+
+        }
+    }
+    // ViewHolder
+    public class PhotoViewHolder : RecyclerView.ViewHolder
+    {
+        //References to ImageView and Textview to be displayed within the CardView in each row of the RecyclerView
+        public ImageView Image { get; private set; }
+        public TextView Caption { get; private set; }
+
+        //References the imageView and textView defined in CardView layout
+        public PhotoViewHolder(View itemView) : base(itemView)
+        {
+            Image = itemView.FindViewById<ImageView>(Resource.Id.imageView);
+            Caption = itemView.FindViewById<TextView>(Resource.Id.textView);
+        }
+    }
+    // Adapter
+    public class PhotoAlbumAdapter : RecyclerView.Adapter
+    {
+        //Instantiates PhotoAlbum into PhotoAlbumAdapter
+        public PhotoAlbum mPhotoAlbum;
+        // Loads the PhotoAlbumAdapter with the PhotoAlbum
+        public PhotoAlbumAdapter (PhotoAlbum photoAlbum)
+        {
+            mPhotoAlbum = photoAlbum;
+        }
+
+        //Using the LayoutManager, Creates a new photo in the CardView
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            //Inflates the CardView for the photo
+            View itemView = LayoutInflater.From(parent.Context).
+                        Inflate(Resource.Layout.PhotoCardView, parent, false);
+            //Create a ViewHolder to find and hold these view references
+            PhotoViewHolder vh = new PhotoViewHolder(itemView);
+            return vh;
+        }
+
+        //Using the LayoutManager, fills the contents of the photo card
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            PhotoViewHolder vh = holder as PhotoViewHolder;
+            // Load the photo image resource from the photo album:
+            vh.Image.SetImageResource(mPhotoAlbum[position].PhotoID);
+            // Load the photo caption from the photo album:
+            vh.Caption.Text = mPhotoAlbum[position].Caption;
+        }
+
+        //Returns number of the photos in PhotoAlbum
+        public override int ItemCount
+        {
+            get { return mPhotoAlbum.NumPhotos; }
+        }
+    }
+}
---
+++ RecyclerView_Example/PhotoAlbum.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+using System.Collections.Generic;
+using RecyclerView_Example;
+
+namespace RecyclerViewer
+{
+    // Photo Class containing ID and Caption
+    public class Photo
+    {
+        public int mPhotoID;
+        public string mCaption;
+
+        public int PhotoID
+        {
+            get { return mPhotoID; }
+        }
+        public string Caption
+        {
+            get { return mCaption; }
+        }
+    }
+
+    // Photo album: holds all image IDs and Captions
+    public class PhotoAlbum
+    {
+        static Photo[] mBuiltInPhotos = {
+            new Photo { mPhotoID = Resource.Drawable.AbstractBlocks,
+                        mCaption = "Abstract Blocks" },
+            new Photo { mPhotoID = Resource.Drawable.AdventureTimeRainbow,
+                        mCaption = "Adventure Time Rainbow" },
+            new Photo { mPhotoID = Resource.Drawable.BetterCallSaul,
+                        mCaption = "Better Call Saul"},
+            new Photo { mPhotoID = Resource.Drawable.BreakingBad,
+                        mCaption = "Breaking Bad"},
+            new Photo { mPhotoID = Resource.Drawable.CalvinAndHobbes,
+                        mCaption = "Calvin and Hobbes"},
+            new Photo { mPhotoID = Resource.Drawable.CityNight,
+                        mCaption = "City at Night"},
+            new Photo { mPhotoID = Resource.Drawable.CloudyOcean,
+                        mCaption = "Cloudy Ocean"},
+            new Photo { mPhotoID = Resource.Drawable.FrogOnLeaf,
+                        mCaption = "Frog on a Leaf"},
+            new Photo { mPhotoID = Resource.Drawable.GreenForest,
+                        mCaption = "Green Forest"},
+            new Photo { mPhotoID = Resource.Drawable.GreenLeaves,
+                        mCaption = "GreenLeaves"},
+            new Photo { mPhotoID = Resource.Drawable.LizardJesus,
+                        mCaption = "Lizard Jesus"},
+            new Photo { mPhotoID = Resource.Drawable.LowResPokemon,
+                        mCaption = "32 Bit Pokemon"},
+            new Photo { mPhotoID = Resource.Drawable.Pier,
+                        mCaption = "Pier"},
+            new Photo { mPhotoID = Resource.Drawable.PlanetsAlign,
+                        mCaption = "Planets Align"},
+            new Photo { mPhotoID = Resource.Drawable.PlanetsAlignIllustrated,
+                        mCaption = "Planets Align Illustrated"},
+            new Photo { mPhotoID = Resource.Drawable.Rapture,
+                        mCaption = "Rapture Bioshock"},
+            new Photo { mPhotoID = Resource.Drawable.RoundHallway,
+                        mCaption = "Round Hallway"},
+            new Photo { mPhotoID = Resource.Drawable.SeaAndRocks,
+                        mCaption = "Sea and Rocks"},
+            new Photo { mPhotoID = Resource.Drawable.SkeletonJesus,
+                        mCaption = "Skeleton Jesus"},
+            new Photo { mPhotoID = Resource.Drawable.SnowyForest,
+                        mCaption = "Snowy Forest"},
+            new Photo { mPhotoID = Resource.Drawable.SnowyLandscape,
+                        mCaption = "Snowy Landscape"},
+            new Photo { mPhotoID = Resource.Drawable.SnowyMountain,
+                        mCaption = "Snowy Mountain"},
+            new Photo { mPhotoID = Resource.Drawable.SpaceCats,
+                        mCaption = "Space Cats"},
+            new Photo { mPhotoID = Resource.Drawable.TrainSmoke,
+                        mCaption = "Train Smoke"},
+            new Photo { mPhotoID = Resource.Drawable.TreeOnLake,
+                        mCaption = "Tree on Lake"},
+            new Photo { mPhotoID = Resource.Drawable.UprightBear,
+                        mCaption = "Grizzly Bear"},
+            new Photo { mPhotoID = Resource.Drawable.WinnieThePooh,
+                        mCaption = "Winnie The Pooh"},
+            new Photo { mPhotoID = Resource.Drawable.YellowBrickRoad,
+                        mCaption = "Yellow Brick Road"},
+            };
+
+        // Array of photos to make up album
+        private Photo[] mPhotos;
+        // Create an instance copy of the built-in photo list and
+        public PhotoAlbum()
+        {
+            mPhotos = mBuiltInPhotos;
+        }
+
+        // Returns the number of photos in the photo album:
+        public int NumPhotos
+        {
+            get { return mPhotos.Length; }
+        }
+
+        //// Indexer for accessing a photo:
+        public Photo this[int i]
+        {
+            get { return mPhotos[i]; }
+        }
+    }
+}
